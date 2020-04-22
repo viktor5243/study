ae69a3011ed8b78f77758d914ed607e997f52064
viktor5243 <62487580+viktor5243@users.noreply.github.com>
2020-04-22 13:14:02 +0900

    Add files via upload

---
+++ hello/src/main/java/head/first/java/GameHelper.java
@@ -0,0 +1,20 @@
+package head.first.java;
+import java.io.*;
+
+public class GameHelper {
+
+	public String getUserInput(String prompt) {
+		String inputLine = null;
+		System.out.println(prompt + " ");
+		try {
+			BufferedReader is = new BufferedReader(
+			new InputStreamReader(System.in));
+			
+			if(inputLine.length() == 0) return null;
+			
+		} catch (IOException e) {
+			System.out.println("IOException" +e);
+		}
+		return inputLine;
+	}
+}
---
+++ hello/src/main/java/head/first/java/SimpleDotComGame.java
@@ -0,0 +1,24 @@
+package head.first.java;
+
+public static void main(String[] args) {
+
+	int numOfGusses = 0;
+	GameHelper helper = new GameHelper();
+	
+	SimpleDotCom theDotCom = new SimpleDotCom();
+	int randomNum = (int)(Math.random()*5);
+	int locations[] = {randomNum, randomNum+1, randomNum+2};
+	theDotCom.setLocationCells(locations);
+	boolean isAlive = true;
+	
+	while(isAlive == true){
+		String guess = helper.getUserInput("enter a number");
+		String result = theDotCom.checkYourself(guess);
+		numOfGusses++;
+		
+		if(result.equals("kill")) {
+			isAlive = false;
+			System.out.println("You took" + numOfGusses + "gusses");
+		}//if•¶‚ÌI—¹
+	}//while•¶‚ÌI—¹
+}//main‚ÌI—¹
